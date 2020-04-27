c002a8df96a6bc1f3b968501b027d0439199a1d3
mg622 <54865166+mg622@users.noreply.github.com>
2020-04-27 19:49:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import Paper from '@material-ui/core/Paper'; 
 import Grid from '@material-ui/core/Grid';
 
@@ -49,21 +50,8 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Nav className="mr-auto"> 
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
   </Navbar.Collapse>
 </Navbar>
     
