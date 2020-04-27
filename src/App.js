fc47ed6647ff890236e78608961935c665f2641b
mg622 <54865166+mg622@users.noreply.github.com>
2020-04-27 19:44:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import {Rating} from "@material-ui/lab";
 
-import Navbar from './Navbar';
-
 import './App.css';
 import Fetcher from './components/Fetcher';
 import Hooks, {aFunc} from './components/Hooks';
@@ -47,6 +45,29 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
 
   return (
     <>
+    <Navbar bg="dark" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    
+    
      <div align = "center">
       <h1>Best Place to Study</h1>
       <p>Fill out the form to rate study areas on campus!</p>
