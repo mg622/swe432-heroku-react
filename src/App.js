cdb51f483c8ae75854d0190a61290375898a5be6
mg622 <54865166+mg622@users.noreply.github.com>
2020-04-27 19:59:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Nav, Navbar, Form, Button } from 'react-bootstrap';
 import Paper from '@material-ui/core/Paper'; 
 import Grid from '@material-ui/core/Grid';
 
@@ -46,14 +45,6 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
 
   return (
     <>
-    <Navbar bg="dark" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto"> 
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
     
     
      <div align = "center">
