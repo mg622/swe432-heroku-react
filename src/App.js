7158e308cbb360c8ce25c2f5d0042c43f8472e78
mg622 <54865166+mg622@users.noreply.github.com>
2020-04-27 20:50:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,25 +15,19 @@ import Hooks, {aFunc} from './components/Hooks';
 
 function App() {
 
-  const [value, setValue] = React.useState('freshman');
-
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   const [loading, setLoading] = useState(true);
-// const [rating, setRating, FW, JC, RHB, SS, VSOE] = useState(3);
-const [ratingJC, setRatingJC] = useState(2.5);
-const [ratingFW, setRatingFW] = useState(2.5);
-const [ratingRHB, setRatingRHB] = useState(2.5);
-const [ratingSS, setRatingSS] = useState(2.5);
-const [ratingVSE, setRatingVSE] = useState(2.5);
+const [ratingNL, setRatingNL] = useState(2.5);
+const [ratingCD, setRatingCD] = useState(2.5);
+const [ratingCM, setRatingCM] = useState(2.5);
 
-const changeJC = (e1, newJC) => { setRatingJC(newJC); };
-const changeFW = (e2, newFW) => { setRatingFW(newFW); };
-const changeRHB = (e3, newRHB) => { setRatingRHB(newRHB); };
-const changeSS = (e4, newSS) => { setRatingSS(newSS); };
-const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
+
+const changeNL = (e1, newNL) => { setRatingJC(newNL); };
+const changeCD = (e2, newCD) => { setRatingFW(newCD); };
+const changeCM = (e3, newCM) => { setRatingRHB(newCM); };
 
 
   useEffect(() => {
@@ -56,27 +50,16 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
               <input type="text" name="name" />
           </label>
         </form>
+        <p></p>
       </div>
     
-    
-      <Grid container direction="column" justify="space-evenly" alignItems="center">
-    <FormControl component="fieldset">
-      <FormLabel component="legend">School Year</FormLabel>
-      <RadioGroup aria-label="schoolYear" name="year1" value={value} onChange={handleChange}>
-        <FormControlLabel value="freshman" control={<Radio />} label="Freshman" />
-        <FormControlLabel value="sophomore" control={<Radio />} label="Sophomore" />
-        <FormControlLabel value="junior" control={<Radio />} label="Junior" />
-        <FormControlLabel value="senior" control={<Radio />} label="Senior" />
-      </RadioGroup>
-    </FormControl> 
-    </ Grid>
 
     <div align = "center">
  <p>Rating for Noise Level (fewer stars means louder): </p>
 
  <Grid container direction="column" justify="space-evenly" alignItems="center">
        <FormLabel component="legend">Noise Level</FormLabel>
-      <Rating name = "Noise Level" id="JC" JC={ratingJC} precision={0.5} defaultValue={2.5} onChange={changeJC} />
+      <Rating name = "Noise Level" id="NL" NL={ratingNL} precision={0.5} defaultValue={2.5} onChange={changeNL} />
     </ Grid>
      <p></p>
   
@@ -84,15 +67,15 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
 
      <Grid container direction="column" justify="space-evenly" alignItems="center">
           <FormLabel component="legend">Crowdedness</FormLabel>
-    <Rating name = "Crowdedness" id="FW" FW={ratingFW} precision={0.5} defaultValue={2.5} onChange={changeFW} />
+    <Rating name = "Crowdedness" id="CD" CD={ratingCD} precision={0.5} defaultValue={2.5} onChange={changeCD} />
     </ Grid>
      <p></p>
   
-  <p>Rating for Comfort of Seats(fewer stars means less comfortable): </p>
+  <p>Rating for Comfort of Seats (fewer stars means less comfortable): </p>
 
      <Grid container direction="column" justify="space-evenly" alignItems="center">
         <FormLabel component="legend">Comfort</FormLabel>
-      <Rating name = "Comfort" id="RHB" RHB={ratingRHB} precision={0.5} defaultValue={2.5} onChange={changeRHB} />
+      <Rating name = "Comfort" id="CM" CM={ratingCM} precision={0.5} defaultValue={2.5} onChange={changeCM} />
       </ Grid>
     <p></p>
 
@@ -100,15 +83,6 @@ const changeVSE = (e5, newVSE) => { setRatingVSE(newVSE); };
 <p></p><p></p>
         <button onclick="printValues()" id="printV">Submit</button> 
   
-<p></p>
-SCHOOL YEAR: {value}
-<p></p>
-JC: {ratingJC}
-<p></p>
-FW: {ratingFW}
-<p></p>
-RHB: {ratingRHB} 
-<p></p>
       </div>
 
     </>
