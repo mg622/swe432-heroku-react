78a861d261b22bfaf5ffcc4048cb51b93cae7e68
mg622 <54865166+mg622@users.noreply.github.com>
2020-05-09 21:08:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ class App extends Component {
     if(event.target.name === "loc") {
       loc = event.target.value;
       var theLoc = document.getElementById('theLoc');
-	  theLoc.style.display='block';
     }
   };
 
@@ -87,73 +86,70 @@ class App extends Component {
           Assignment 7 - Mei Gibbons
     			</p>
     			<p>
-          <br/>
-    			Please fill out the following survey to the best of your ability and answer all of the questions.
-    			<br/>
     			</p>
         </div>
 
     			<div id = "formQuestions" align = "center">
     			
-    		Location
+    		<strong>Location</strong>
     	    <br/>
-            	<input type="text" onChange={this.handleChange} id="theLoc" name="loc" style={{display:'none'}}  />
+            	<input type="text" onChange={this.handleChange} id="theLoc" name="loc" style={{display:'block'}}  />
     	    <br/>
     			
     			
-            Rating for Noise Level: (1-very loud, 5-silent)
+            <strong>Rating for Noise Level:</strong> (1-very loud, 5-silent)
             <br/>
 
     	      <input type="radio" onChange={this.handleChange} id="one" name="noiseLevel" value="1" />
-    	      <label htmlFor="one">1</label><br/>
+    	      <label htmlFor="one">1</label> &nbsp
     	      
     	      <input type="radio"  onChange={this.handleChange} id="two" name="noiseLevel" value="2" />
-    	      <label htmlFor="two">2</label><br/>
+    	      <label htmlFor="two">2</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="three" name="noiseLevel" value="3" />
-    	      <label htmlFor="three">3</label><br/>
+    	      <label htmlFor="three">3</label> &nbsp
     	      
     	      <input type="radio"  onChange={this.handleChange} id="four" name="noiseLevel" value="4" />
-    	      <label htmlFor="four">4</label><br/>
+    	      <label htmlFor="four">4</label> &nbsp
     	      
     	      <input type="radio"  onChange={this.handleChange} id="five" name="noiseLevel" value="5" />
     	      <label htmlFor="five">5</label><br/>
     	    
     	    <br/>
 
-    	      Rating for Crowdedness: (1-way too crowded, 5-hardly any people)
+    	      <strong>Rating for Crowdedness:</strong> (1-way too crowded, 5-hardly any people)
     	      <br/>
 
     	      <input type="radio" onChange={this.handleChange} id="one" name="crowd" value="1"/>
-    	      <label htmlFor="one">1</label><br/>
+    	      <label htmlFor="one">1</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="two" name="crowd" value="2"/>
-    	      <label htmlFor="two">2</label><br/>
+    	      <label htmlFor="two">2</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="three" name="crowd" value="3"/>
-    	      <label htmlFor="three">3</label><br/>
+    	      <label htmlFor="three">3</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="four" name="crowd" value="4"/>
-    	      <label htmlFor="four">4</label><br/>
+    	      <label htmlFor="four">4</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="five" name="crowd" value="5"/>
     	      <label htmlFor="five">5</label><br/>
     	      <br/>
 
-    	      Rating for Comfort of Seats: (1-uncomfortable, 5-very comfortable)
+    	      <strong>Rating for Comfort of Seats:</strong> (1-uncomfortable, 5-very comfortable)
     	      <br/>
 
     	      <input type="radio" onChange={this.handleChange} id="one" name="comfort" value="1"/>
-    	      <label htmlFor="one">1</label><br/>
+    	      <label htmlFor="one">1</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="two" name="comfort" value="2"/>
-    	      <label htmlFor="two">2</label><br/>
+    	      <label htmlFor="two">2</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="three" name="comfort" value="3"/>
-    	      <label htmlFor="three">3</label><br/>
+    	      <label htmlFor="three">3</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="four" name="comfort" value="4"/>
-    	      <label htmlFor="four">4</label><br/>
+    	      <label htmlFor="four">4</label> &nbsp
     	      
     	      <input type="radio" onChange={this.handleChange} id="five" name="comfort" value="5"/>
     	      <label htmlFor="five">5</label><br/>
@@ -161,7 +157,7 @@ class App extends Component {
 
 
             <Button onClick={this.fetchData} color="primary" data-something="submit">
-                 submit</Button>
+                 submit</Button> <br>
 
     			</div>
           <div align = "center">
